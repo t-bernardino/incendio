c837dfa98a1e6c16f6b1c92bf35a3dbfd0157392
t-bernardino <bernardinotulio@gmail.com>
2022-01-28 05:26:27 -0300

    mmelhoras. dataset melhor

--- R/incendio.R
+++ R/incendio.R
@@ -1,9 +1,14 @@
+#' @export
+
+
 library(tidyverse)
 
 incendiosPorEstado <- function(){
-  ggplot(data = incendios_UC, mapping = aes(incendios_UC$ANO,..count..)) +
+
+  registro_incendios %>% filter(ANO > 2014) %>% filter(ANO < 2021) %>%
+  ggplot(aes(ANO,..count..)) +
     geom_bar(fill = "#e0441d") +
-    labs(title = "Incêndios em unidades de conservação por ano",
+    labs(title = "Ocorrências de incêndio registradas por ano",
          x = "Ano",
          y = "Número de incêndios")
 
